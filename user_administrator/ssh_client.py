6b524ad6da6225fda26eef69bd441c13add8230f
Mr Gene <yzhydovich@gmail.com>
2020-03-28 20:44:05 -0500

    implemented Create and Delete functions

--- user_administrator/ssh_client.py
+++ user_administrator/ssh_client.py
@@ -1,40 +1,58 @@
-import paramiko, os, string, pprint, socket, traceback, sys
+import paramiko, socket, sys
 
-def create_user(host, key_filename, current_user):
+def create_user(host, key_filename, new_user, username):
     rsa_key = paramiko.RSAKey.from_private_key_file(key_filename)
     ssh = paramiko.SSHClient()
+    ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
     try:
-        ssh.connect(host, pkey=rsa_key, username=current_user)
-        print("Connected to Server: ", host, "User: ", current_user)
-        ssh_stdin, ssh_stdout, ssh_stderr = ssh.exec_command('ls')
-        print("Result")
-        print(ssh_stdout.read())
-        print("Errors")
-        print(ssh_stderr.read())
+        ssh.connect(host, pkey=rsa_key, username=username)
+        print("Connected to Server:", host, "User:", username)
+        print("Creating user: " + new_user)
+
+        stdin, stdout, stderr = ssh.exec_command('sudo useradd ' + new_user)
+        decoded_stdout = stdout.read().decode('ascii').strip("\n")
+        decoded_stderr = stderr.read().decode('ascii').strip("\n")
+        if len(decoded_stdout) != 0:
+            print("Result: " + decoded_stdout)
+        if len(decoded_stderr) != 0:
+            print("Error: " + decoded_stderr)
+        ssh.close()
+        print('Closing connection')
     except paramiko.AuthenticationException:
-        print("Authentication problem - Server: ", host, "User: ", current_user)
+        sys.exit("Error: Authentication problem - Server: " + host + " User: " + username)
     except socket.error:
-        print("Comunication problem - Server: ", host, "User: ", current_user)
-    ssh.close()
+        sys.exit("Error: Comunication problem - Server: " + host + " User: " + username)
 
 def list_users(host, key_filename, username):
     rsa_key = paramiko.RSAKey.from_private_key_file(key_filename)
     ssh = paramiko.SSHClient()
 
     ssh.connect(host, pkey=rsa_key)
-    ssh_stdin, ssh_stdout, ssh_stderr = ssh.exec_command('ls')
+    stdin, stdout, stderr = ssh.exec_command('less /etc/passwd')
     print("Result")
-    print(ssh_stdout.read())
+    print(stdout.read())
     print("Errors")
-    print(ssh_stderr.read())
+    print(stderr.read())
 
-def delete_user(host, key_filename, username):
+def delete_user(host, key_filename, delete_user, username):
     rsa_key = paramiko.RSAKey.from_private_key_file(key_filename)
     ssh = paramiko.SSHClient()
 
-    ssh.connect(host, pkey=rsa_key)
-    ssh_stdin, ssh_stdout, ssh_stderr = ssh.exec_command('ls')
-    print("Result")
-    print(ssh_stdout.read())
-    print("Errors")
-    print(ssh_stderr.read())
+    ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+    try:
+        ssh.connect(host, pkey=rsa_key, username=username)
+        print("Connected to Server:", host, "User:", username)
+        print("Deleting user: " + delete_user)
+        stdin, stdout, stderr = ssh.exec_command('sudo userdel ' + delete_user)
+        decoded_stdout = stdout.read().decode('ascii').strip("\n")
+        decoded_stderr = stderr.read().decode('ascii').strip("\n")
+        if len(decoded_stdout) != 0:
+            print("Result: " + decoded_stdout)
+        if len(decoded_stderr) != 0:
+            print("Error: " + decoded_stderr)
+        ssh.close()
+        print('Closing connection')
+    except paramiko.AuthenticationException:
+        sys.exit("Error: Authentication problem - Server: " + host + " User: " + username)
+    except socket.error:
+        sys.exit("Error: Comunication problem - Server: " + host + " User: " + username)
--- user_administrator/user_administrator.py
+++ user_administrator/user_administrator.py
@@ -3,9 +3,7 @@ import getpass
 from .ssh_client import create_user, list_users, delete_user
 
 def create_user_middleman(args):
-    print(args)
-    current_user=getpass.getuser()
-    create_user(host=args.host, key_filename=args.sshkey, current_user=current_user)
+    create_user(host=args.host, key_filename=args.ssh_key, new_user=args.new_user, username=args.username)
 
 def list_users_middleman(args):
     print('"list()" called')
@@ -13,8 +11,7 @@ def list_users_middleman(args):
 
 
 def delete_user_middleman(args):
-    print('"delete()" called')
-    print(args.sshkey)
+    delete_user(host=args.host, key_filename=args.ssh_key, delete_user=args.delete_user, username=args.username)
 
 
 parser = argparse.ArgumentParser(description="Helps with administration of users on a specified host")
@@ -23,23 +20,62 @@ subparsers = parser.add_subparsers(dest='create/list/delete', required=True, hel
 # Create
 parser_create = subparsers.add_parser('create', help='Creates a user on a specified host')
 parser_create.set_defaults(func=create_user_middleman)
-parser_create.add_argument("--host", type=str, required=True, help="Host address")
-parser_create.add_argument("--sshkey", type=str, required=True, help="SHH key file")
-parser_create.add_argument("--new-user", "-nu", type=str, required=True, help='User to be created')
+parser_create.add_argument("--host",
+                            type=str, 
+                            required=True,
+                            help="Host address")
+parser_create.add_argument("--ssh-key",
+                            type=str,
+                            required=True,
+                            help="SHH key file")
+parser_create.add_argument("--new-user", "-nu",
+                            type=str,
+                            required=True,
+                            help='User to be created')
+parser_create.add_argument("--username",
+                            type=str,  
+                            help="Specific user for the ssh session, if different from the active user")
 
 # List
 parser_list = subparsers.add_parser('list', help='Returns a list of users on a specified host')
 parser_list.set_defaults(func=list_users_middleman)
-parser_list.add_argument("--host", type=str, required=True, help="Host address")
-parser_list.add_argument("--sshkey", type=str, required=True, help="SHH key file")
+parser_create.add_argument("--host",
+                            type=str, 
+                            required=True,
+                            help="Host address")
+parser_create.add_argument("--ssh-key",
+                            type=str,
+                            required=True,
+                            help="SHH key file")
+parser_create.add_argument("--username",
+                            type=str,  
+                            help="Specific user for the ssh session, if different from the active user")
+# think of verbose output
 
 # Delete
 parser_delete = subparsers.add_parser('delete', help='Deletes a user on a specified host')
 parser_delete.set_defaults(func=delete_user_middleman)
-parser_delete.add_argument("--host", type=str, required=True, help="Host address")
-parser_delete.add_argument("--sshkey", type=str, required=True, help="SHH key file")
+parser_delete.add_argument("--host",
+                            type=str, 
+                            required=True,
+                            help="Host address")
+parser_delete.add_argument("--ssh-key",
+                            type=str,
+                            required=True,
+                            help="SHH key file")
+parser_delete.add_argument("--delete-user", "-du",
+                            type=str,
+                            required=True,
+                            help='User to be deleted')
+parser_delete.add_argument("--username",
+                            type=str,  
+                            help="Specific user for the ssh session, if different from the active user")
 
 args = parser.parse_args()
+
+if not args.username:
+    args.username = getpass.getuser()
+
 args.func(args)
 
 # type=argparse.FileType('r')
