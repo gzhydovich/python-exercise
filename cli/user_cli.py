94734ed5de2c60c4ba77927ff86c8f2ea01b0ab1
Mr Gene <yzhydovich@gmail.com>
2020-03-29 13:25:30 -0500

    refactor parsers

--- cli/user_cli.py
+++ cli/user_cli.py
@@ -3,61 +3,86 @@ import getpass
 from .ssh_client import create_user, list_users, delete_user
 
 parser = argparse.ArgumentParser(description="Helps with administration of users on a specified host")
-subparsers = parser.add_subparsers(dest='create/list/delete', required=True, help='Comands')
+subparsers = parser.add_subparsers(
+
+    help='Available commands',
+    required=True,
+)
 
 # Create
 parser_create = subparsers.add_parser('create', help='Creates a user on a specified host')
 parser_create.set_defaults(func=create_user)
-parser_create.add_argument("--host",
-                            type=str, 
-                            required=True,
-                            help="Host address")
-parser_create.add_argument("--ssh-key",
-                            type=str,
-                            required=True,
-                            help="SHH key file")
-parser_create.add_argument("--new-user", "-nu",
-                            type=str,
-                            required=True,
-                            help='User to be created')
-parser_create.add_argument("--username",
-                            type=str,  
-                            help="Specific user for the ssh session, if different from the active user")
+parser_create.add_argument(
+    "--host",
+    help="Host address",
+    type=str, 
+    required=True,
+)
+parser_create.add_argument(
+    "--ssh-key",
+    help="SHH key file",
+    type=str,
+    required=True,
+)
+parser_create.add_argument(
+    "--new-user", "-nu",
+    help='User to be created',
+    type=str,
+    required=True,
+)
+parser_create.add_argument(
+    "--username",
+    help="Specific user for the ssh session, if different from the active user",
+    type=str,  
+)
 
 # List
 parser_list = subparsers.add_parser('list', help='Returns a list of users on a specified host')
 parser_list.set_defaults(func=list_users)
-parser_list.add_argument("--host",
-                            type=str, 
-                            required=True,
-                            help="Host address")
-parser_list.add_argument("--ssh-key",
-                            type=str,
-                            required=True,
-                            help="SHH key file")
-parser_list.add_argument("--username",
-                            type=str,  
-                            help="Specific user for the ssh session, if different from the active user")
-# think of verbose output
+parser_list.add_argument(
+    "--host",
+    help="Host address",
+    type=str, 
+    required=True,
+)
+parser_list.add_argument(
+    "--ssh-key",
+    help="SHH key file",
+    type=str,
+    required=True,
+)
+parser_list.add_argument(
+    "--username",
+    help="Specific user for the ssh session, if different from the active user",
+    type=str,  
+)
 
 # Delete
 parser_delete = subparsers.add_parser('delete', help='Deletes a user on a specified host')
 parser_delete.set_defaults(func=delete_user)
-parser_delete.add_argument("--host",
-                            type=str, 
-                            required=True,
-                            help="Host address")
-parser_delete.add_argument("--ssh-key",
-                            type=str,
-                            required=True,
-                            help="SHH key file")
-parser_delete.add_argument("--delete-user", "-du",
-                            type=str,
-                            required=True,
-                            help='User to be deleted')
-parser_delete.add_argument("--username",
-                            type=str,  
-                            help="Specific user for the ssh session, if different from the active user")
+parser_delete.add_argument(
+    "--host",
+    help="Host address",
+    type=str, 
+    required=True,
+)
+parser_delete.add_argument(
+    "--ssh-key",
+    help="SHH key file",
+    type=str,
+    required=True,
+)
+parser_delete.add_argument(
+    "--delete-user", "-du",
+    help='User to be deleted',
+    type=str,
+    required=True,
+)
+parser_delete.add_argument(
+    "--username",
+    help="Specific user for the ssh session, if different from the active user",
+    type=str,  
+)
 
 args = parser.parse_args()
 
