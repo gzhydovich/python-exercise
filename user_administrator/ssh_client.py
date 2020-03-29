d62d625834bf2b73b2ca8289e9560af0f0e61ee6
Mr Gene <yzhydovich@gmail.com>
2020-03-29 11:45:22 -0500

    add proper awk formatting for list function

--- user_administrator/ssh_client.py
+++ user_administrator/ssh_client.py
@@ -9,26 +9,26 @@ def create_user(host, key_filename, new_user, username):
         decoded_stdout = stdout.read().decode('ascii').strip("\n")
         decoded_stderr = stderr.read().decode('ascii').strip("\n")
         if len(decoded_stderr) != 0:
-            if "warning" in decoded_stderr:
                 print(decoded_stderr)
-            else:
-                print("Error: " + decoded_stderr)
         if len(decoded_stdout) != 0:
-            print("Result: " + decoded_stdout)
+            print(decoded_stdout)
+
         print('Closing connection')
         ssh.close()
 
 def list_users(host, key_filename, username):
         ssh = ssh_session(host, key_filename, username)
 
-        _, stdout, stderr = ssh.exec_command('sudo less /etc/passwd ')
-        decoded_stdout = stdout.read().decode('ascii').strip("\n")
+        awk_command = "awk -F: \'BEGIN{printf \"%-20s %-20s %-20s\\n\", \"USERNAME:\", \"ID:\", \"NAME:\"}; { printf \"%-20s %-20s %-20s\\n\", $1, $3, $5 }\' /etc/passwd"
+        _, stdout, stderr = ssh.exec_command(awk_command)
+        decoded_stdout = stdout.read().decode('ascii')
         decoded_stderr = stderr.read().decode('ascii').strip("\n")
         if len(decoded_stderr) != 0:
-            print("Error: " + decoded_stderr)
+            print(decoded_stderr)
         if len(decoded_stdout) != 0:
-            print("Result:\n" + decoded_stdout)
-        print('Closing connection')
+            print(decoded_stdout)
+
+        print('\nClosing connection')
         ssh.close()
 
 def delete_user(host, key_filename, delete_user, username):
@@ -39,9 +39,10 @@ def delete_user(host, key_filename, delete_user, username):
         decoded_stdout = stdout.read().decode('ascii').strip("\n")
         decoded_stderr = stderr.read().decode('ascii').strip("\n")
         if len(decoded_stderr) != 0 :
-            print("Error: " + decoded_stderr)
+            print(decoded_stderr)
         if len(decoded_stdout) != 0:
-            print("Result: " + decoded_stdout)
+            print(decoded_stdout)
+
         print('Closing connection')
         ssh.close()
 
@@ -55,5 +56,6 @@ def ssh_session(host, key_filename, username):
         return ssh
     except paramiko.AuthenticationException:
         sys.exit("Error: Authentication problem - Server: " + host + " User: " + username)
-    except socket.error:
-        sys.exit("Error: Comunication problem - Server: " + host + " User: " + username)
+    except socket.error as e:
+        print("Error: Comunication problem - Server: " + host + " User: " + username)
+        sys.exit(e)
